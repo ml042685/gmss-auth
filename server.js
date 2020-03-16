e03207d30d36d09ea24fd7330fb2d373222628e3
James Goff <james_goff88@outlook.com>
2020-03-16 18:35:35 -0500

    update server.js

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(
-    db,
-    { useNewUrlParser: true }
+    db, {
+      useNewUrlParser: true
+    }
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
@@ -31,4 +32,13 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
