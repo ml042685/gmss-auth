9d8b96379748fd27fb284b502f80e57069025ca4
James Goff <james_goff88@outlook.com>
2020-03-17 18:53:58 -0500

    update path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require("path");
 require('dotenv').config();
 
 const app = express();
